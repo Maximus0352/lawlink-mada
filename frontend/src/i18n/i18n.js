07e13aac208a3a24120efbf003189cad16a3a095
max <ferdinomax1@gmail.com>
2026-05-10 17:21:23 +0300

    feat(frontend): configuration de la traduction multilingue

---
+++ frontend/src/i18n/i18n.js
@@ -0,0 +1,24 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import fr from './fr.json';
+import mg from './mg.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      fr: { translation: fr },
+      mg: { translation: mg },
+    },
+    fallbackLng: 'fr',
+    lng: localStorage.getItem('ll_lang') || 'fr',
+    interpolation: { escapeValue: false },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
+  });
+
+export default i18n;
